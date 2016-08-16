e4e60c5528c147ecac0528d74490a9086920d8a7
kin0025 <alex@akinrosssmith.id.au>
2016-08-17 00:29:07 +1000

    Prettied up the scoreboard interface

--- scoreboard.c
+++ scoreboard.c
@@ -12,7 +12,7 @@
 
 #define INITIAL_SCORE 0
 #define SCRBRD_OFFSET 1
-
+#define SCRBRD_WIDTH 29
 /**
  * initalise the scoreboard so that the scores for each element of the array
  * is set to 0.
@@ -20,10 +20,9 @@
 void init_scoreboard(score scores[MAX_SCORES]) {
     int i;
     struct player temp;
-    strcpy(temp.name, "Name");
+    strcpy(temp.name, "");
     temp.score = 0;
     temp.token = BLANK;
-    printf("%s %d",temp.name,temp.score);
     for (i = 0; i < MAX_SCORES; i++) {
 
         scores[i] = temp;
@@ -52,10 +51,21 @@ BOOLEAN add_to_scoreboard(score scores[MAX_SCORES], struct player *winner) {
  **/
 void display_scores(score scores[MAX_SCORES]) {
     int i;
-    printf("=== SCOREBOARD ===\n==================\n");
+    printf("\n=== %sSCOREBOARD%s ===\n==================\n",MENU_COLOUR,COLOR_RESET);
+    for(i=0;i<SCRBRD_WIDTH;i++){
+        printf("-");
+    }
+    printf("\n|Name                 |Score|\n");
+    for(i=0;i<SCRBRD_WIDTH;i++){
+        printf("-");
+    }
+    printf("\n");
     for (i = 0; i < MAX_SCORES; i++) {
         if (scores[i].score!=0) {
-            printf("|%-20s | %2d |\n\n", scores[i].name, scores[i].score);
+            printf("|%-20s | %2d  |\n", scores[i].name, scores[i].score);
+        }else if(i==0){
+            printf("No-one has won a game! Go play some games and check back.");
+            break;
         }
     }
 }
